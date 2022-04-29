2d6057cbc0011e6537ff8817dbc6233cc6f5fbed
Byron Yego <byronyego@gmail.com>
2022-04-29 10:24:52 +0300

    is this initial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,24 @@ import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext; //locks up the component tree for the first time
   render() {
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context; //destructuring the context
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.bg, color: theme.syntax }}>
-        <h1>Contex App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.bg, color: theme.syntax }}>
+              <h1>Contex App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
