1a77580bebd04ab6da5af4cb20abe0c26c20fd2f
Byron Yego <byronyego@gmail.com>
2022-05-25 13:27:02 +0300

    use context in a functional component using useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context; //destructuring
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list">
-        <ul style={{ background: theme.bg, color: theme.syntax }}>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
-
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
+};
 export default BookList;
